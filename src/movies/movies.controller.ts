f144eb9bef806975562a6fe474c9adf61e5e70bb
ehdnd_desktop <taeungjang@naver.com>
2025-11-02 01:10:15 +0900

    2 0 movie controller

---
+++ src/movies/movies.controller.ts
@@ -0,0 +1,38 @@
+import {
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+  Put,
+} from '@nestjs/common';
+
+// 엔트리 포인트 컨트롤
+@Controller('movies')
+export class MoviesController {
+  @Get()
+  getAll() {
+    return 'This will return all movies.';
+  }
+
+  @Get('/:id')
+  getOne(@Param('id') movieId: string) {
+    return `This will return one movie with the id: ${movieId}.`;
+  }
+
+  @Post()
+  create() {
+    return 'This will create a movie.';
+  }
+
+  @Delete('/:id')
+  delete(@Param('id') movieId: string) {
+    return `This will delete a movie with the id: ${movieId}`;
+  }
+
+  @Patch('/:id')
+  patchOne(@Param('id') movieId: string) {
+    return `This will patch a movie with the id: ${movieId}`;
+  }
+}
