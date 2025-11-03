345bddbef3d607aefeb1b3eb311a136d638578b1
ehdnd_mac <taeungjang@naver.com>
2025-11-03 17:27:10 +0900

    3 3 testing deleteOne and create

--- src/movies/movies.service.spec.ts
+++ src/movies/movies.service.spec.ts
@@ -42,4 +42,40 @@ describe('MoviesService', () => {
       expect(() => service.getOne(999)).toThrow('Movie with ID 999 not found.');
     });
   });
+
+  describe('deleteOne', () => {
+    it('should deletes a movie', () => {
+      service.create({
+        title: 'Test Movie',
+        genres: ['test'],
+        year: 2000,
+      });
+
+      const allMovies = service.getAll();
+      service.deleteOne(1);
+      const afterDeleteAllMovies = service.getAll();
+
+      expect(afterDeleteAllMovies.length).toEqual(allMovies.length - 1);
+    });
+
+    it('should throw 404 error', () => {
+      expect(() => service.deleteOne(999)).toThrow(NotFoundException);
+    });
+  });
+
+  describe('create', () => {
+    it('should create a movie', () => {
+      const beforeCreateAllMoviesLengh = service.getAll().length;
+      service.create({
+        title: 'Test Movie',
+        genres: ['test'],
+        year: 2000,
+      });
+      const afterCreateAllMoviesLenth = service.getAll().length;
+
+      expect(afterCreateAllMoviesLenth).toBeGreaterThan(
+        beforeCreateAllMoviesLengh,
+      );
+    });
+  });
 });
