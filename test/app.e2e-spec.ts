adfc06ad02b5bc61d2c823f9156d548528b4a13f
ehdnd_mac <taeungjang@naver.com>
2025-11-03 19:25:43 +0900

    4 1 testing GET movies id

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -1,5 +1,5 @@
 import { Test, TestingModule } from '@nestjs/testing';
-import { INestApplication } from '@nestjs/common';
+import { INestApplication, ValidationPipe } from '@nestjs/common';
 import request from 'supertest';
 import { App } from 'supertest/types';
 import { AppModule } from './../src/app.module';
@@ -7,12 +7,21 @@ import { AppModule } from './../src/app.module';
 describe('AppController (e2e)', () => {
   let app: INestApplication<App>;
 
-  beforeEach(async () => {
+  // DB가 계속 동일했으면 좋겠어
+  beforeAll(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
       imports: [AppModule],
     }).compile();
 
+    // e2e 환경도 실제 main.ts 처럼 동일하게 Pipe를 적용해야한다
     app = moduleFixture.createNestApplication();
+    app.useGlobalPipes(
+      new ValidationPipe({
+        whitelist: true,
+        forbidNonWhitelisted: true,
+        transform: true,
+      }),
+    );
     await app.init();
   });
 
@@ -41,5 +50,18 @@ describe('AppController (e2e)', () => {
     it('(DELETE)', () => {
       return request(app.getHttpServer()).delete('/movies').expect(404);
     });
+  });
+
+  describe('/movies/:id', () => {
+    it('(GET)', () => {
+      return request(app.getHttpServer()).get('/movies/1').expect(200);
+    });
+
+    it('(GET) 404', () => {
+      return request(app.getHttpServer()).get('/movies/111').expect(404);
+    });
+
+    it.todo('(PATCH)');
+    it.todo('(DELETE)');
   });
 });
